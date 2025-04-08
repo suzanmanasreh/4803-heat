5c740f02031db1a1ab321f3a2ecc2e8060581d45
smanasreh6 <smanasreh6@gatech.edu>
2025-04-08 15:53:00 -0400

    init so far

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 parallel:
-	mpicxx main_2d_par.cpp -o main_2d_par.o
+	mpicxx main_2d_par.cpp -I/opt/homebrew/Cellar/boost/1.87.0_1/include -L/opt/homebrew/Cellar/boost/1.87.0_1/lib -o main_2d_par.o
 seq:
 	mpicxx main_2d_seq.cpp -o main_2d_seq.o
 clean:
--- main_2d_par.cpp
+++ main_2d_par.cpp
@@ -8,18 +8,31 @@
 #include <iomanip>
 #include <sstream>
 #include <mpi.h>
+#include "boost/multi_array.hpp"
+
 
 using namespace std;
 
-void print_x(vector<vector<double>> x, double time);
-void output_to_file(vector<vector<double>> x, int step, int x_dim, int y_dim);
+typedef boost::multi_array<double, 2> arr_2d;
+typedef arr_2d::index idx;
+
+void print_x(arr_2d x, double time);
+void output_to_file(arr_2d x, int step, int x_dim, int y_dim);
 
 template <typename T>
 T square(T num) {
     return num * num;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    int rank, p;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm comm = MPI_COMM_WORLD;
+
+    MPI_Comm_size(comm, &p);
+    MPI_Comm_rank(comm, &rank);
+
     double t1, t2;
     double init_inner = 10.0;
     double init_border = 0;
@@ -39,8 +52,24 @@ int main() {
     double w_out = 1;
     double writes = 0;
 
-    int x_total = x_dim + 2;
-    int y_total = y_dim + 2;
+    int x_domains = 2;
+    int y_domains = 2;
+
+    int num_domains = x_domains * y_domains;
+
+    if (rank == 0 && p != x_domains * y_domains) {
+        printf("Warning: Mismatch between number of processors %d and number of domains %d\n", p, num_domains);
+    }
+
+    int x_global = x_dim + 2;
+    int y_global = y_dim + 2;
+
+    int x_total = x_global + (2 * x_domains);
+    int y_total = y_global + (2 * y_domains);
+
+    if (rank == 0) {
+        printf("x_total: %d, y_total: %d\n", x_total, y_total);
+    }
 
     double min_h = dx;
     if (dy < dx) min_h = dy;
@@ -49,71 +78,100 @@ int main() {
     // printf("dx: %f, dy: %f, min_h: %f, max_dt: %f\n", dx, dy, min_h, max_dt);
 
     if (dt >= max_dt) {
-        printf("dt too large. setting it to %f\n", max_dt);
+        if (rank == 0) {
+            printf("dt too large. setting it to %f\n", max_dt);
+        }
         dt = max_dt;
     }
 
-    vector<vector<double>> x(x_total, vector<double> (y_total, init_inner));
-    vector<vector<double>> prev(x_total, vector<double> (y_total, init_inner));
-    // printf("init_inner: %lf\n", init_inner);
 
-    for (int i = 0; i < x_total; i++) {
+
+    arr_2d x(boost::extents[x_total][y_total]);
+    arr_2d prev(boost::extents[x_total][y_total]);
+
+    fill(x.data(), x.data() + x.num_elements(), init_inner);
+    fill(prev.data(), prev.data() + prev.num_elements(), init_inner);
+
+    // print_x(x, curr_time);
+
+    // initilize borders to border values
+    for (idx i = 0; i < x_total; i++) {
         x[i][0] = init_border;
         x[i][y_total - 1] = init_border;
     }
 
-    for (int j = 0; j < y_total; j++) {
+    for (idx j = 0; j < y_total; j++) {
         x[0][j] = init_border;
         x[x_total - 1][j] = init_border;
     }
 
-    // print_x(x, curr_time);
-    output_to_file(x, 0, x_dim, y_dim);
-
-    double sx = (alpha * dt) / (dx * dx);
-    double sy = (alpha * dt) / (dy * dy);
-
-    t1 = MPI_Wtime();
-
-    for (int k = 1; k <= num_steps; k++) {
-        double diff = 0;
-        for (int i = 1; i <= x_dim; i++) {
-            for (int j = 1; j <= y_dim; j++) {
-                prev[i][j] = x[i][j];
-                x[i][j] += (sx * (x[i + 1][j] - (2*x[i][j]) + x[i - 1][j])) + (sy * (x[i][j + 1] - (2*x[i][j]) + x[i][j - 1]));
-                diff += square(x[i][j] - prev[i][j]);
-            }
-        }
-        curr_time += dt;
-        // print_x(x, curr_time);
-        output_to_file(x, k, x_dim, y_dim);
-        if (diff < .01) {
-            printf("convergence at step %d\n", k);
-            break;
-        }
+    // initialize ghost cells next to border to border values
+    for (idx i = 1; i < x_total - 1; i++) {
+        x[i][1] = init_border;
+        x[i][y_total - 2] = init_border;
     }
 
-    t2 = MPI_Wtime();
+    for (idx j = 1; j < y_total - 1; j++) {
+        x[1][j] = init_border;
+        x[x_total - 2][j] = init_border;
+    }
 
-    double tot_time = t2 - t1;
-    printf("total time: %f\n", tot_time);
+    if (rank == 0) {
+        print_x(x, curr_time);
+        output_to_file(x, 0, x_dim, y_dim);
+    }
+
+    vector<int> x_start(p);
+    vector<int> x_end(p);
+    vector<int> y_start(p);
+    vector<int> y_end(p);
+
+    // double sx = (alpha * dt) / (dx * dx);
+    // double sy = (alpha * dt) / (dy * dy);
+
+    // t1 = MPI_Wtime();
+
+    // for (int k = 1; k <= num_steps; k++) {
+    //     double diff = 0;
+    //     for (int i = 1; i <= x_dim; i++) {
+    //         for (int j = 1; j <= y_dim; j++) {
+    //             prev[i][j] = x[i][j];
+    //             x[i][j] += (sx * (x[i + 1][j] - (2*x[i][j]) + x[i - 1][j])) + (sy * (x[i][j + 1] - (2*x[i][j]) + x[i][j - 1]));
+    //             diff += square(x[i][j] - prev[i][j]);
+    //         }
+    //     }
+    //     curr_time += dt;
+    //     // print_x(x, curr_time);
+    //     output_to_file(x, k, x_dim, y_dim);
+    //     if (diff < .01) {
+    //         printf("convergence at step %d\n", k);
+    //         break;
+    //     }
+    // }
+
+    // t2 = MPI_Wtime();
+
+    // double tot_time = t2 - t1;
+    // printf("total time: %f\n", tot_time);
+
+    MPI_Finalize();
 
 }
 
 // print our grid of values
-void print_x(vector<vector<double>> x, double time) {
+void print_x(arr_2d x, double time) {
     printf("curr_time: %f\n", time);
-    for(int i = 0; i < x.size(); i++){
-		for(int j = 0; j < x[i].size(); j++) {
+    for(idx i = 0; i < x.size(); i++){
+		for(idx j = 0; j < x[i].size(); j++) {
             printf("%5.02f ", x[i][j]);
         }
         printf("\n");
 	}
 }
 
-void output_to_file(vector<vector<double>> x, int step, int x_dim, int y_dim) {
+void output_to_file(arr_2d x, int step, int x_dim, int y_dim) {
     ostringstream file_name;
-    file_name << "2d/ex_";
+    file_name << "2dp/ex_";
     file_name << setw(4) << setfill('0') << step;
     file_name << ".vtk";
 
